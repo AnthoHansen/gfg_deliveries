e04ab50b7d438de56fe67816b859b2a99b64b6d8
AnthoHansen <62178203+AnthoHansen@users.noreply.github.com>
2024-08-16 00:57:57 +0100

    Create es.lua

---
+++ locale/es.lua
@@ -0,0 +1,45 @@
+Locale.es = {
+    -- Localización Pre-Inicio
+    interactionPrompt = "Comenzar %s Entrega", -- El primer %s es el tipo de entrega.
+    menuTitle = "Entregas de %s", -- La clave de la tabla del tipo de entrega en la configuración.
+    stops = "Paradas",
+    stopsDescription = "Cantidad máxima de paradas que podrías recibir en tu ruta",
+    boxes = "Cajas por parada",
+    boxesDescription = "Cantidad máxima de cajas que podrías tener por parada",
+    vehicle = "Vehículo proporcionado",
+    vehicleDescription = "El vehículo que se te proporciona para tu ruta",
+    takeJob = "Comenzar Entrega",
+    takeJobDescription = "Comenzar Entrega con %s", -- El primer %s es el tipo de entrega.
+    endJob = "Terminar Entrega",
+    endJobDescription = "Terminar Entrega con %s", -- El primer %s es el tipo de entrega.
+    noSpawn = "No se encontró espacio libre",
+    noSpawnDescription = "Espera hasta que haya un espacio disponible para el camión",
+
+    -- Localización Inicio del trabajo
+    jobStarted = "Has comenzado una ruta, dirígete a tu vehículo y comienza a entregar. Tienes %s paradas para completar.", -- El primer %s es la cantidad de paradas asignadas.
+    dropoffBlip = "Entrega #%s", -- El primer %s es el número de la parada.
+    dropoffAssigned = "Dirígete a la parada marcada en tu GPS.",
+
+    -- Localización en la Parada
+    atDropoff = "Has llegado a la parada",
+    atDropoffDescription = "Tienes %s paquetes para entregar", -- El primer %s es la cantidad de cajas para esta parada.
+    grabBox = "Tomar paquete",
+    hasBox = "Ya tienes un paquete en la mano",
+    hasBoxDescription = "Entrégalo para obtener otro",
+    deliverBox = "Entregar Paquete",
+    boxDelivered = "Paquete Entregado",
+    boxDeliveredDescription = "Dirígete al camión y toma otro, te quedan %s.",
+    nextDropoff = "Parada Completada",
+    nextDropoffDescription = "Dirígete a la siguiente parada marcada en tu GPS, te quedan %s",
+    allBoxesDelivered = "No hay más cajas",
+    allBoxesDeliveredDescription = "Dirígete a la siguiente parada marcada en tu GPS o regresa con el encargado.",
+    noBox = "Sin paquete",
+    noBoxDescription = "Necesitas tomar un paquete del camión",
+
+    -- Localización Fin del Trabajo
+    stopsDone = "Todas las Paradas Completadas",
+    stopsDoneDescription = "Regresa con el encargado marcado en tu GPS para recoger tu pago",
+    stopCount = "Paradas Completadas",
+    boxCount = "Paquetes Entregados",
+    distanceCount = "Distancia Recorrida"
+}
